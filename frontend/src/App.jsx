a7c019f847a34b5ce30ab4f671707084bdc801f2
Manickam11 <221401049@rajalakshmi.edu.in>
2025-03-04 05:50:29 +0530

    git commit -m "Added map component and styling"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ChatGPTUser from './components/ChatGPTUser';
+// import ChatGPTUser from './components/ChatGPTUser';
+import Map from './components/map';
 
 function App() {
   return (
-      <ChatGPTUser />
+      <Map />
   );
 }
 
